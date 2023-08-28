d9ee8b4b7b280c84fe1e660e3b31dd426b75f3a6
Ági <raczagi07@gmail.com>
2023-08-28 19:33:45 +0200

    connect

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,10 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-public class Main {
+import java.sql.*;
+
+public class ConnectionFactory {
 
     public static String felhasznalo = System.getenv("name");
     public static String adatbazis = System.getenv("url");
@@ -18,8 +14,24 @@ public class Main {
             throw new RuntimeException("Error connecting to the database", e);
         }
     }
+
     public static void main(String[] args) {
 
+      Connection connection = getConnection();
+        try (
+                Statement statement = connection.createStatement();
+
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM felhasznalo");
+        ) {
+            while ((resultSet.next())) {
+
+                System.out.println(resultSet.getString("name"));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+
+        }
 
 
 
